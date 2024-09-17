62fd28ffeec3c9abce6818ab1eb6f67e332874c4
YOGEESH K P <yogeesh793@gmail.com>
2024-09-18 01:51:14 +0530

    Add files via upload

---
+++ Checking_even_number.py
@@ -0,0 +1,6 @@
+# write a program for checking whether the given number is even or not program
+a=int(input("enter the value:" ))
+if a%2==0:
+    print("a is even number")
+else:
+    print("a is not even number")
